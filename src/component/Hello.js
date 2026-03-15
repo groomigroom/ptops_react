a499695ef82ef12a03da57490d6bf9675285b19b
groomigroom <googjohn0143@gmail.com>
2026-03-15 11:49:08 +0000

    prosage

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -3,10 +3,11 @@ import { useState } from "react";
 export default function Hello(props){
     console.log(props);
     const [name, setName] = useState("Groom");
+    const [age, setAge] =useState(props.age);
 
     return (
         <div>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">{name}({props.age})</h2>
             <button
                 onClick={() => {
                     setName(name === "Groom" ? "mungmung" : "Groom");
